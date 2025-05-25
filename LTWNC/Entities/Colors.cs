6577d6d893c3b571137d0712b22dfc753696bc4f
Ngô Chí Tấn <ngochitan456@gmail.com>
2025-05-25 10:24:14 +0700

    feat/CRUDColorandSize

--- LTWNC/Entities/Colors.cs
+++ LTWNC/Entities/Colors.cs
@@ -1,6 +1,21 @@
-﻿namespace LTWNC.Entities
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+
+namespace LTWNC.Entities
 {
     public class Colors
     {
+        [BsonId]
+        [BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("colors_name")]
+        [JsonPropertyName("colors_name")]
+        public string? ColorsName { get; set; } // ví dụ: "Black"
+
+        [BsonElement("colors_code")]
+        [JsonPropertyName("colors_code")]
+        public string? ColorsCode { get; set; } // ví dụ: "#000000"
     }
 }
--- LTWNC/Entities/Sizes.cs
+++ LTWNC/Entities/Sizes.cs
@@ -1,6 +1,18 @@
-﻿namespace LTWNC.Entities
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace LTWNC.Entities
 {
     public class Sizes
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("size_name")]
+        public string? SizesName { get; set; }
+
+        [BsonElement("sizes_code")]
+        public string? SizesCode { get; set; }
     }
 }
