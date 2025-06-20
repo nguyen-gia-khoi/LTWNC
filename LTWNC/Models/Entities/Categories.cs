17e230205f60d0a38f7d46309be8dd7a0ae475f6
Ngô Chí Tấn <ngochitan456@gmail.com>
2025-06-20 18:27:34 +0700

    feat/categories

---
+++ LTWNC/Models/Entities/Categories.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+
+namespace LTWNC.Models.Entities
+{
+    public class Categories
+    {
+        [BsonId]
+        [BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("categories_name")]
+        [JsonPropertyName("categories_name")]  // 👈 JSON cần có field "colors_name"
+        public string? CategoriesName { get; set; }
+
+        [BsonElement("categories_code")]
+        [JsonPropertyName("categories_code")]  // 👈 JSON cần có field "colors_code"
+        public string? CategoriesCode { get; set; }
+    }
+}
